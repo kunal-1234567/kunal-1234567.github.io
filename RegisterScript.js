857b55f5cb82e2b6f5b53905765ed0e3f1975644
Kunal Gupta <kg6992040@gmail.com>
2025-08-14 22:30:07 +0530

    init new repo

---
+++ RegisterScript.js
@@ -0,0 +1,9 @@
+function checkAge() {
+  let age = document.getElementById("ageInput").value;
+
+  if (age >= 16) {
+    window.location.href = "LoginPage.html"; 
+  } else {
+    message.textContent = "Sorry, you must be at least 16 years old to create account."; 
+  }
+}
