15739f91a2c1c955d19c14d2e8277dbf7392fc5a
Kunal Gupta <kg6992040@gmail.com>
2025-08-16 10:33:38 +0530

    changes in home.html and rejister.js

--- register.js
+++ register.js
@@ -2,7 +2,7 @@ function checkAge() {
   let age = document.getElementById("ageInput").value;
 
   if (age >= 16) {
-    window.location.href = "login.html"; 
+    window.location.href = "index.html"; 
   } else {
     message.textContent = "Sorry, you must be at least 16 years old to create account."; 
   }
