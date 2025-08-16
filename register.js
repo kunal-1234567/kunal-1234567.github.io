eb5620d6f84f2e4cb42fc8b4f35dd246fbdf08f4
Himanshu Gupta <himanshugupta@Himanshus-MacBook-Pro.local>
2025-08-16 13:18:43 +0530

    chore: added validation and restrcure changes

--- register.js
+++
@@ -1,9 +0,0 @@
-function checkAge() {
-  let age = document.getElementById("ageInput").value;
-
-  if (age >= 16) {
-    window.location.href = "index.html"; 
-  } else {
-    message.textContent = "Sorry, you must be at least 16 years old to create account."; 
-  }
-}
